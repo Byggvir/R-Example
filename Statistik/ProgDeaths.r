dceba6efe96e900df7a5dbb09c91cadfb85d691e
Byggvir <thomas@arend-rhb.de>
2020-08-04 22:34:14 +0200

    Uses function curve

---
+++ Statistik/ProgDeaths.r
@@ -0,0 +1,12 @@
+#!/usr/bin/r
+
+population <- data.readcsv("../data/DEPopulationAge.csv")
+CFR <- data.readcsv("../data/DE_CFR.csv")
+
+aCFR <- rep(0,101)
+
+for ( i in 1:101 ) {
+  aCFR[i] <- CFR$Deaths_m+[i % 10+1]/10
+}
+
+print (aCFR)
--- Statistik/Vorhersagekraft.r
+++ Statistik/Vorhersagekraft.r
@@ -2,29 +2,47 @@
 
 png(file="Vorhersagekraft.png",width=1920,height=1080)
 
-p <- seq(from=0.000,to=0.005,by=0.0001)
-
 f <- c(0.0001,0.0002,0.0005,0.001,0.002,0.005,0.01,0.02,0.05)
 
-plot (  p * 100
-      , p
+vk <- function (x,f) {
+
+  return( x*(1-f) / ( (1-f)*x + (1-x)*f ) )
+
+}
+
+xabs <- c(0,2)
+
+plot (  NA
+      , NA
       , type="n"
+      , xlim=xabs
       , ylim=c(0,100)
       , xlab="Prävalenz [%]"
       , ylab="Vorhersagekraft [%]"
       , main="Vorhersagekraft eines positiven Tests"
       , sub="Vorhersagekraft nach Prävalenz und Testfehler"
     )
+    
 grid()
+
 for (i in 1:length(f)) {
-   
-    v <- p*(1-f[i]) /((1-f[i])*p+(1-p)*f[i])
-    lines( p * 100
-         , v * 100
+
+  par(new=TRUE) 
+  curve(vk(x/100,f[i])*100,
+         , xlim=xabs
+         , ylim=c(0,100)
+         , xaxt="n"
+         , yaxt="n"
          , col=i
          , type="l"
+         , xlab=""
+         , ylab=""
+         
          )
+
+
 }
+
 
 legend( "bottomright"
       , legend=paste(f*100," %")
@@ -32,7 +50,7 @@ legend( "bottomright"
       , col=1:length(f)
       , lty=rep(1,length(f))
       , bg=rgb(224/256,224/256,192/256)
-      , cex=2
+      , cex=1
       )
 
 abline(h=90,lty=3)
