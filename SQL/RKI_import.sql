64e0abee3db6042a8c884d2c4ecd13f878f975d4
Byggvir <thomas@arend-rhb.de>
2020-10-09 22:06:20 +0200

    Record numbering in CSV output included

--- SQL/RKI_import.sql
+++ SQL/RKI_import.sql
@@ -1,8 +1,12 @@
 use COVID19;
-select "Date","Kw","WTag","Cases","Deaths","incCases","incDeaths"
+
+set @i:=0;
+
+select "Date","Day","Kw","WTag","Cases","Deaths","incCases","incDeaths"
 union all
 select 
     t1.day as Date
+    , (@i:=@i+1) as Day
     , WEEK(t1.day,1) as Kw
     , WEEKDAY(t1.day) as WTag
     , t1.cases as Cases
@@ -14,10 +18,13 @@ inner join rki as t2
 on t1.day=adddate(t2.day,1)
 into OUTFILE '/tmp/RKI_nach_Tag.csv' FIELDS TERMINATED by ',';
 
-select "Kw","Tage","Cases","Deaths"
+set @i=0;
+
+select "Woche","Kw","Tage","Cases","Deaths"
 union all
 select 
-    week(t1.day) as Kalenderwoche
+    (@i:=@i+1) as Woche
+    , week(t1.day) as Kalenderwoche
     , count(week(t1.day)) as Tage
     , sum(t1.cases-t2.cases) as Cases
     , sum(t1.deaths-t2.deaths) as Deaths
