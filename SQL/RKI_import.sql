a45e21ada0e79efdfa32cf593ed4818dfa4fa168
Byggvir <thomas@arend-rhb.de>
2020-10-11 01:25:21 +0200

    Start calender week on Monday and count from 1 with more tahn 3 days in year

--- SQL/RKI_import.sql
+++ SQL/RKI_import.sql
@@ -7,7 +7,7 @@ union all
 select 
     t1.day as Date
     , (@i:=@i+1) as Day
-    , WEEK(t1.day,1) as Kw
+    , WEEK(t1.day,3) as Kw
     , WEEKDAY(t1.day) as WTag
     , t1.cases as Cases
     , t1.deaths as Deaths
@@ -24,12 +24,12 @@ select "Woche","Kw","Tage","Cases","Deaths"
 union all
 select 
     (@i:=@i+1) as Woche
-    , week(t1.day) as Kalenderwoche
+    , week(t1.day,3) as Kalenderwoche
     , count(week(t1.day)) as Tage
     , sum(t1.cases-t2.cases) as Cases
     , sum(t1.deaths-t2.deaths) as Deaths
     from rki as t1
     inner join rki as t2 
     on t1.day=adddate(t2.day,1)
-    group by week(t1.day)
+    group by week(t1.day,3)
 into OUTFILE '/tmp/RKI_nach_Kw.csv' FIELDS TERMINATED by ',';
