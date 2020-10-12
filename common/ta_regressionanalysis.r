0cd598f6574df595c378318b7ec5b85bdf30a7f2
Byggvir <thomas@arend-rhb.de>
2020-10-12 10:28:18 +0200

    Add parameter xlim, ylim parameters

--- common/ta_regressionanalysis.r
+++ common/ta_regressionanalysis.r
@@ -2,7 +2,7 @@
 # ---- Zeichnen des Ergebnisses einer exponentiellen Regressiosnanalyse auf einem Plot
 #
 
-plotregression <- function( a , b, xlim = c(0,1),  linecol = c("green","orange","red")) {
+plotregression <- function( a , b, xlim = c(0,1), ylim = c(0,3), linecol = c("green","orange","red")) {
   
   for (i in 1:3 ) {
     par (new = TRUE)    
@@ -16,17 +16,18 @@ plotregression <- function( a , b, xlim = c(0,1),  linecol = c("green","orange",
            , xlim = xlim
            , ylim = ylim
            , lwd = 3
+           , lty = 3
     )
     text( xlim[2]
           , exp(a[i]+b[i]*xlim[2])
           , round(exp(a[i]+b[i]*xlim[2]),0)
           , col = linecol[i]
           , adj = 0
-          , cex = 1
+          , cex = 2
     )
     
   }
   
 }
 
-# ----11
+# ----
