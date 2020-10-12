eae8e7405740205d3e67096b9ff7d812adc93fe3
Byggvir <thomas@arend-rhb.de>
2020-10-12 10:27:27 +0200

    Updated data

--- SQL/RKI_import.sql
+++ SQL/RKI_import.sql
@@ -20,14 +20,16 @@ into OUTFILE '/tmp/RKI_nach_Tag.csv' FIELDS TERMINATED by ',';
 
 set @i=0;
 
-select "Woche","Kw","Tage","Cases","Deaths"
+select "Woche","Kw","Tage","Cases","Deaths","WMin","WMax"
 union all
 select 
     (@i:=@i+1) as Woche
     , week(t1.day,3) as Kalenderwoche
-    , count(week(t1.day)) as Tage
+    , count(week(t1.day,3)) as Tage
     , sum(t1.cases-t2.cases) as Cases
     , sum(t1.deaths-t2.deaths) as Deaths
+    , min(t1.cases-t2.cases) as WMin
+    , max(t1.cases-t2.cases) as WMax 
     from rki as t1
     inner join rki as t2 
     on t1.day=adddate(t2.day,1)
