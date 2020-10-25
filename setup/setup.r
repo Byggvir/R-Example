17047fc384c7ec9a5c74eddd1e3b40d30af68a41
Byggvir <thomas@arend-rhb.de>
2020-10-25 15:38:18 +0100

    First commit

---
+++ setup/setup.r
@@ -0,0 +1,13 @@
+install.packages("data.table")
+install.packages("grid")
+install.packages("gridExtra")
+install.packages("readODS")
+install.packages("readr")
+install.packages("REST")
+install.packages("RMariaDB")
+install.packages("rvest")
+install.packages("scales")
+install.packages("stringr")
+install.packages("tidyverse")
+install.packages("utils")
+
